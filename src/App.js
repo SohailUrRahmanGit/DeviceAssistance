f265d0dac07e52d717c8c12eb266e8470108b25b
SOHAIL UR RAHMAN SHAIK <sohail-ur.rahman@eviden.com>
2024-02-08 23:15:22 +0530

    Completed adding description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import 'bulma/css/bulma.css'
 import ProfileCardComponent from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortnaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
-    return (<div>
-        <div> Personal Assistance Types </div>
-        <ProfileCardComponent title="Siri" handle="@siri99"/>
-        <ProfileCardComponent title="Alexa" handle="@alexa99"/>
-        <ProfileCardComponent title="okGoogle" handle="@okGoogl"/>
-    </div>
+    return (
+        <div>
+            <section class="hero is-primary">
+                <div class="hero-body">
+                    <p class="title">
+                       List Of Digital Assistance
+                    </p>
+                </div>
+            </section>
+            <div className='container'>
+                <div className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCardComponent desc="Siri" handle="@siri99" image={SiriImage} 
+                              descText="Siri is used in iOS and its the fastest of all"
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCardComponent desc="Alexa" handle="@alexa99" image={AlexaImage}
+                                descText="Alexa is used in android and its second  fastest of all"
+                                />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCardComponent desc="okGoogle" handle="@okGoogl" image={CortnaImage}
+                            descText="Not so fast still work in progress"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCardComponent({desc, handle,image}) {
+function ProfileCardComponent({desc, handle,image,descText}) {
     return(
     <div className="card">
         <div className="card-image">
@@ -8,11 +8,13 @@ function ProfileCardComponent({desc, handle,image}) {
         </div>
      <div className="card-content">
         <div className="media-content">
-         <div className="title is -4"> {desc} </div>
-         <div className="subtitle is -6"> {handle} </div>
+         <div className="title is -4"> Title is {desc} </div>
+         <div className="subtitle is -6"> handle is {handle} </div>
+         <div className="subtitle is -6"> {descText} </div>
+
         </div>
      </div>
-    </div>
+</div>
     );
 }
 
